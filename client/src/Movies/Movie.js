f8d6c53e1432d7dd90470032615c94b4e2bc5e4c
Nick Coultas <nncoultas@yahoo.com>
2018-03-12 12:37:55 -0700

    Finished All Steps

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
+import { Link } from 'react-router-dom';
 export default class MovieCard extends React.Component {
   
   state = {
@@ -9,7 +9,7 @@ export default class MovieCard extends React.Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => this.setState(() => ({ movie: response.data })))
@@ -26,6 +26,7 @@ export default class MovieCard extends React.Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="movie-card">
+        <Link to = "/">Home</Link>
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
+const MovieRoutes = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path = "/" component={ MovieList } />
+        <Route exact path = "/movies/:id" component={ Movie } />
+      </div>
+    </Router>  
+  )
+}
+
+
+
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <div>
+    <MovieRoutes />
+  </div>,
   document.getElementById('root')
 );
